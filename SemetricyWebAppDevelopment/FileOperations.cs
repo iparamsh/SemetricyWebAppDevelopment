b6602f58a95c6d68c7ab12c22edf851f50e69092
anarchysmo <illyaiphone7@gmail.com>
2022-07-29 21:50:06 -0700

    Add project files.

---
+++ SemetricyWebAppDevelopment/FileOperations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace SemetricyWebAppDevelopment
+{
+    public class FileOperations
+    {
+        public void createFolder(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                System.IO.Directory.CreateDirectory(filePath);
+            }
+        }
+
+        public string choosePathToTheFolder()
+        {
+            string path = "";
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    path = fbd.SelectedPath;
+                }
+            }
+
+            return path;
+        }
+
+        public void createNewProject(string path, string name)
+        {
+            if (Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path + "\\" + name);
+            }
+            else
+                System.Windows.Forms.MessageBox.Show("Folder does not exist");
+        }
+
+        public bool checkIfCorrectPathForOpenFile(string path)
+        {
+            if (Directory.Exists(path))
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ SemetricyWebAppDevelopment/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.IO;
+
+namespace SemetricyWebAppDevelopment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FileOperations fileOperation = new FileOperations();
+        private string pathToDir = System.IO.Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents/Semetricy");
+
+        //private string sFileName = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileOperation.createFolder(pathToDir);
+            pathToProject.Text = pathToDir;
+            pathToOpenProject.Text = pathToDir;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RoutedEventArgs e)
+        {
+           string url = @"https://discord.gg/sCtYruVkCC";
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+        }
+
+        private void browsePathToNewProject(object sender, RoutedEventArgs e)
+        {
+            pathToProject.Text = fileOperation.choosePathToTheFolder();
+        }
+
+        private void BrowsePathToTheExistantProj_click(object sender, RoutedEventArgs e)
+        {
+            pathToOpenProject.Text = fileOperation.choosePathToTheFolder();
+        }
+
+        private void createProjectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (nameOfTheProject.Text != "" && pathToProject.Text != "")
+            {
+                fileOperation.createNewProject(pathToProject.Text, nameOfTheProject.Text);
+            }
+            else
+            {
+                if (nameOfTheProject.Text == "")
+                    MessageBox.Show("You cant leave name of the project empty!!!");
+                else
+                    MessageBox.Show("You cant leave path to the project empty!!!");
+            }
+        }
+
+        private void openProjectBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (pathToOpenProject.Text != "" && fileOperation.checkIfCorrectPathForOpenFile(pathToOpenProject.Text))
+            {
+                MessageBox.Show("sucess");
+                //...
+            }
+            else
+            {
+                if (pathToOpenProject.Text == "")
+                {
+                    MessageBox.Show("Can't leave the path root empty!");
+                }
+                else
+                {
+                    MessageBox.Show("Path is incorrect!");
+                }
+            }
+        }
+    }
+}
